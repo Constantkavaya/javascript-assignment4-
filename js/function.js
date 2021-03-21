9228b710fc417d04aa99c8b0288611a6f5175caa
Constantkavaya <kavayaconstant@gmail.comgit config --listgit config --global user.email kavayaconstant@gmail.com>
2021-03-21 11:26:00 +0300

    first commit

---
+++ js/function.js
@@ -0,0 +1,43 @@
+
+
+// var fruit={
+//     fruitname:'orange',
+//     price:30.00,
+//     quantity:2,
+// }
+  function fruitname(name,quantity){
+
+       var price=30.00*quantity;
+       var c= quantity + " " +name +" KES "+  price.toFixed(2);
+       console.log(c);
+
+   }
+   fruitname("Oranges", 2);
+
+
+   /*
+
+   let door={
+     type:"steel",
+     colour:"green"
+     shape:"rectangular",
+      
+   door.buy=function(){
+     alert("durable");
+   }
+    door.buy();
+   
+   function door(){
+     var type="steel"
+     var colour="green"
+     var shape="rectangular"
+     function buy(){
+       var conclusion="colour+ " " +steel+" "+shape+ " "door",
+     }
+console.log(conclusion);
+
+   }
+   buy()
+   
+   */
+   
